6a42f406154e2fa6b8df73c0736264b0750beb5c
KluDan <KluDan@users.noreply.github.com>
2024-02-09 11:00:42 +0000

    deploy: 7e586c4385431fc828f58cbb1ec880a290e0fb33

---
+++ assets/Catalog-eyWCU3mM.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-IhG3tdS3.js";function o(){return t.jsx("div",{children:"Catalog"})}export{o as default};
---
+++ assets/Favourites-LTxYhoS8.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-IhG3tdS3.js";const s=()=>r.jsx("div",{children:"Favourites"});export{s as default};
